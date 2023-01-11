22a61bd5b215e14a0a4e6f79ced1417499411323
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2023-01-12 01:56:36 +0300

    feat: doctrine upgrade

--- src/BackendBase/Shared/Factory/Doctrine/ConnectionFactory.php
+++ src/BackendBase/Shared/Factory/Doctrine/ConnectionFactory.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace BackendBase\Shared\Factory\Doctrine;
 
 use Doctrine\DBAL\Configuration;
-use Doctrine\DBAL\Driver\Connection;
+use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\DriverManager;
 use Doctrine\DBAL\Types\Type;
 use Doctrine\DBAL\Types\VarDateTimeImmutableType;
@@ -15,7 +15,7 @@ use Laminas\ServiceManager\Factory\FactoryInterface;
 
 class ConnectionFactory implements FactoryInterface
 {
-    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null) : Connection
+    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null): Connection
     {
         Type::overrideType('datetime', VarDateTimeType::class);
         Type::overrideType('datetimetz', VarDateTimeType::class);
@@ -24,6 +24,7 @@ class ConnectionFactory implements FactoryInterface
         Type::overrideType('datetime_immutable', VarDateTimeImmutableType::class);
         Type::overrideType('datetimetz_immutable', VarDateTimeImmutableType::class);
         Type::overrideType('time_immutable', VarDateTimeImmutableType::class);
+
         $config           = $container->get('config');
         $connectionParams = $config['doctrine']['dbal'];
         $doctrineConfig   = new Configuration();
