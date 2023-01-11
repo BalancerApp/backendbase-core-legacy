3c7ab9102c4f22398c5e4d08d6b92b48179b50a3
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2023-01-12 01:54:01 +0300

    feat: doctrine upgrade

--- src/BackendBase/Shared/Factory/Doctrine/EntityManagerFactory.php
+++ src/BackendBase/Shared/Factory/Doctrine/EntityManagerFactory.php
@@ -4,22 +4,22 @@ declare(strict_types=1);
 
 namespace BackendBase\Shared\Factory\Doctrine;
 
-use Symfony\Component\Cache\Adapter\ArrayAdapter as ArrayCache;
-use Symfony\Component\Cache\Adapter\PhpFilesAdapter as PhpFileCache;
 use Doctrine\Common\Cache\Psr6\DoctrineProvider;
-use Doctrine\DBAL\Driver\Connection;
+use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Types\Type;
 use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping\Driver\AttributeDriver;
-use Interop\Container\ContainerInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
+use Psr\Container\ContainerInterface;
 use Ramsey\Uuid\Doctrine\UuidType;
 use Scienta\DoctrineJsonFunctions\Query\AST\Functions\Postgresql as DqlFunctions;
+use Symfony\Component\Cache\Adapter\ArrayAdapter as ArrayCache;
+use Symfony\Component\Cache\Adapter\PhpFilesAdapter as PhpFileCache;
 
-final class EntityManagerFactory implements FactoryInterface
+class EntityManagerFactory implements FactoryInterface
 {
-    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null) : EntityManager
+    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null): EntityManager
     {
         $appConfig = $container->get('config');
         if ($appConfig['debug'] === true) {
@@ -27,11 +27,12 @@ final class EntityManagerFactory implements FactoryInterface
         } else {
             $cache = DoctrineProvider::wrap(new PhpFileCache());
         }
+
         Type::addType('uuid', UuidType::class);
         $client      = $container->get(Connection::class);
         $doctrineDir = $appConfig['app']['data_dir'] . '/cache/Doctrine';
         $config      = new Configuration();
-        $driverImpl  = $this->newDefaultAttributeDriver('src/Infrastructure/Persistence/Doctrine/Entity');
+        $driverImpl  = $this->newDefaultAttributeDriver(['src/Infrastructure/Persistence/Doctrine/Entity']);
         $config->setMetadataCacheImpl($cache);
         $config->setProxyDir($doctrineDir . '/Proxies');
         $config->setProxyNamespace($appConfig['doctrine']['namespace-for-generator'] . '\\Proxies');
@@ -39,10 +40,12 @@ final class EntityManagerFactory implements FactoryInterface
         $config->addCustomStringFunction(DqlFunctions\JsonGetText::FUNCTION_NAME, DqlFunctions\JsonGetText::class);
         $config->addCustomStringFunction(DqlFunctions\JsonGet::FUNCTION_NAME, DqlFunctions\JsonGet::class);
         $config->setMetadataDriverImpl($driverImpl);
+        $config->setResultCacheImpl($cache);
 
         return EntityManager::create($client, $config);
     }
-       private function newDefaultAttributeDriver($paths = []): AttributeDriver
+
+    private function newDefaultAttributeDriver($paths = []): AttributeDriver
     {
            return new AttributeDriver($paths);
     }
