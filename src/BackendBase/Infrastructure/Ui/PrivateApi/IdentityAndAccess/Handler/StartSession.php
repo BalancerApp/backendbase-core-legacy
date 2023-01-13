168e1b4228e1f0ee5a6b7e95d0412aa0b7000306
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2023-01-13 10:38:10 +0300

    fix: login jwt issue

--- src/BackendBase/Infrastructure/Ui/PrivateApi/IdentityAndAccess/Handler/StartSession.php
+++ src/BackendBase/Infrastructure/Ui/PrivateApi/IdentityAndAccess/Handler/StartSession.php
@@ -59,13 +59,13 @@ class StartSession implements RequestHandlerInterface
         }
 
         $signer = new Sha256();
-        $time   = time();
+        $time   = new \DateTimeImmutable();
         $token  = (new Builder())->issuedBy('storage') // Configures the issuer (iss claim)
         //    ->permittedFor('http://example.org') // Configures the audience (aud claim)
         //    ->identifiedBy('4f1g23a12aa', true) // Configures the id (jti claim), replicating as a header item
         ->issuedAt($time) // Configures the time that the token was issue (iat claim)
         ->canOnlyBeUsedAfter($time) // Configures the time that the token can be used (nbf claim)
-        ->expiresAt($time + 60*60*12) // Configures the expiration time of the token (exp claim)
+        ->expiresAt($time->add(new \DateInterval('PT12H'))) // Configures the expiration time of the token (exp claim)
         ->withClaim('userId', $user->id()->toString()) // Configures a new claim, called "uid"
         ->withClaim('role', $user->role()) // Configures a new claim, called "uid"
         ->getToken($signer, new Key('d81c8751fdd0a01e62b7acac5bea23a0d7d29beb03e428b863d02376aea628c1'));
@@ -74,8 +74,8 @@ class StartSession implements RequestHandlerInterface
 
         $data = [
             'accessToken' => (string) $token,
-            'createdAt' => $time,
-            'willExpireAt' => $time + 3600,
+            'createdAt' => $time->format(DATE_ATOM),
+            'willExpireAt' => $time->add(new \DateInterval('PT12H'))->format(DATE_ATOM),
             'userData' => [
                 'firstName' => $user->firstName(),
                 'lastName' => $user->lastName(),
